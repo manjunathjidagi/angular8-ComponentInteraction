d1d5f542085520cfeb7ebd2f31cd2509783804a8
Manjunath Jidagi <manjunathjidagi.sony@gmail.com>
2020-04-13 17:36:29 +0530

    * Component Interaction Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'componentInteractionProject';
   public name = "Manjunath"
+  public message = ""
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,15 +6,31 @@ import { Component, OnInit, Input } from '@angular/core';
 
     <h2>{{"Hello "+parentData}}</h2>
 
+    <!--
+    <h2>{{"Hello Again "+name}}</h2>
+    -->
+
+    <button (click)="fireEvent()">Fire</button>
+
   `,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
   @Input() public parentData
+  
+  // or
+  //@Input('parentData') public name
+
+  @Output() public childEvent = new EventEmitter()
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fireEvent(){
+    this.childEvent.emit('Hello.. This is from Child')
   }
 
 }
